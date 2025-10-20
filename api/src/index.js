b61be719d21d7336a62146d13dc0344e1e85075b
Marcos <malome2@jviladoms.cat>
2025-10-20 10:22:57 +0200

    Sessió 5

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes'); // Importem les rutes
 const app = express();
 app.use(express.json());
 connectDB();
-app.get('/', (req, res) => res.send('API Ecommerce en marxa 🚀'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor escoltant al port ${PORT}`));
+app.get('/', (req, res) => res.send('API Ecommerce en marxa'));
+// Muntem les rutes de productes sota el prefix '/api/products'
+app.use('/api/products', productRoutes);
